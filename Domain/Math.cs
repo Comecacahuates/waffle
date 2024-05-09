3495257e5f2238db76b1f18f97fdd795f32dba41
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-09 01:16:47 -0600

    feat(Math): RoundUp method

---
+++ Domain/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waffle.Domain
+{
+    public static class Math
+    {
+        public static double RoundUp(double value, double step)
+        {
+            return System.Math.Ceiling(value / step) * step;
+        }
+    }
+}
