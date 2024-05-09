1fcda3351ad81dc76870fe30435434c620483837
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-09 01:56:13 -0600

    feat: convert slice curves to polylines in Slice class

--- Domain/Slicing/Slice.cs
+++ Domain/Slicing/Slice.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rhino.Geometry;
+using Rhino;
 
 namespace Waffle.Domain.Slicing
 {
     public class Slice
     {
         public Plane Plane { get; private set; }
-        public Curve[] Curves { get; private set; }
+        public PolylineCurve[] Curves { get; private set; }
 
         public Slice(Brep brep, Plane slicingPlane)
         {
             Plane = slicingPlane;
-            Curves = Brep.CreateContourCurves(brep, slicingPlane);
+            Curve[] curves = Brep.CreateContourCurves(brep, slicingPlane);
+            Curves = new PolylineCurve[curves.Length];
+
+            for (int i = 0; i < Curves.Length; i++)
+            {
+                Curves[i] = curves[i].ToPolyline(
+                    RhinoMath.DefaultDistanceToleranceMillimeters,
+                    RhinoMath.DefaultAngleTolerance,
+                    0.01, 10.0);
+            }
         }
     }
 }
