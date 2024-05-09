87956bfdde84c58e48d431b1005da79c3a373254
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-09 01:33:14 -0600

    feat: IntervalDivider class

---
+++ Domain/Math/IntervalDivider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace Waffle.Domain.Math
+{
+    public static class IntervalDivider
+    {
+        public static double[] DivideIntervalInNumbers(Interval interval, int divisionCount)
+        {
+            double step = interval.Length / divisionCount;
+            double[] values = new double[divisionCount];
+
+            for (int i = 0; i < divisionCount + 1; i++)
+                values[i] = interval.T0 + step * i;
+
+            return values;
+        }
+    }
+}
