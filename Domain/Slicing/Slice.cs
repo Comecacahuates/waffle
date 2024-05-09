38eb997b7e0ef7c09abdeda962c4852bbc8aa022
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-09 01:34:23 -0600

    feat: Slice class

---
+++ Domain/Slicing/Slice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace Waffle.Domain.Slicing
+{
+    public class Slice
+    {
+        public Plane Plane { get; private set; }
+        public Curve[] Curves { get; private set; }
+
+        public Slice(Brep brep, Plane slicingPlane)
+        {
+            Plane = slicingPlane;
+            Curves = Brep.CreateContourCurves(brep, slicingPlane);
+        }
+    }
+}
