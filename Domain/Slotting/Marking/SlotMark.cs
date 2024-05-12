33a9ed2d47eb10fc1d394617589605c6504ea4a8
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-12 05:23:57 -0600

    feat: SlotMark class

---
+++ Domain/Slotting/Marking/SlotMark.cs
@@ -0,0 +1,21 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waffle.Domain.Slotting.Marking
+{
+    internal class SlotMark
+    {
+        public int CurveIndex { get; private set; }
+        public Line line { get; private set; }
+
+        public SlotMark(int curveIndex, Line line)
+        {
+            CurveIndex = curveIndex;
+            this.line = line;
+        }
+    }
+}
