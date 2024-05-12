4f6ec471d71aff1f2f2792b21fa659297f46dc6f
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-12 01:38:38 -0600

    feat: SlottingPlaneIntersection class

---
+++ Domain/Slotting/SlottingPlaneIntersection.cs
@@ -0,0 +1,21 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waffle.Domain.Slotting
+{
+    internal class SlottingPlaneIntersection
+    {
+        public Point3d Point { get; private set; }
+        public int CurveIndex { get; private set; }
+
+        public SlottingPlaneIntersection(Point3d point, int curveIndex)
+        {
+            Point = point;
+            CurveIndex = curveIndex;
+        }
+    }
+}
