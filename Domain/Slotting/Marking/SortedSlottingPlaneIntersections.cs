d3707ff6e54433f45a46b7acbe8f98663b0b4950
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-12 05:21:36 -0600

    feat: SortedSlottingPlaneIntersections class

---
+++ Domain/Slotting/Marking/SortedSlottingPlaneIntersections.cs
@@ -0,0 +1,36 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Waffle.Domain.Comparing.Points;
+
+namespace Waffle.Domain.Slotting.Marking
+{
+    internal class SortedSlottingPlaneIntersections : IEnumerable<SlottingPlaneIntersection>
+    {
+        private SortedList<Point3d, SlottingPlaneIntersection> sortedList;
+
+        public IEnumerator<SlottingPlaneIntersection> GetEnumerator()
+        {
+            return sortedList.Values.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public SortedSlottingPlaneIntersections(Plane plane)
+        {
+            VCoordinateComparer comparer = new VCoordinateComparer(plane);
+            sortedList = new SortedList<Point3d, SlottingPlaneIntersection>(comparer);
+        }
+
+        public void Add(SlottingPlaneIntersection intersection)
+        {
+            sortedList.Add(intersection.Point, intersection);
+        }
+    }
+}
