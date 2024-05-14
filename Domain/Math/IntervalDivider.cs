d78291bc067970c225bc4452f07d31178b1671b3
Adrián Juárez <comecacahuates@yahoo.com>
2024-05-14 03:05:14 -0600

    fix(IntervalDivider): number of values in DivideIntervalInNumbers method

--- Domain/Math/IntervalDivider.cs
+++ Domain/Math/IntervalDivider.cs
@@ -7,12 +7,12 @@ using Rhino.Geometry;
 
 namespace Waffle.Domain.Math
 {
-    public static class IntervalDivider
+    internal static class IntervalDivider
     {
         public static double[] DivideIntervalInNumbers(Interval interval, int divisionCount)
         {
             double step = interval.Length / divisionCount;
-            double[] values = new double[divisionCount];
+            double[] values = new double[divisionCount + 1];
 
             for (int i = 0; i < divisionCount + 1; i++)
                 values[i] = interval.T0 + step * i;
